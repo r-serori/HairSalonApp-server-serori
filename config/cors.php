b525211c64e24e4393136c2a7bbdae5caea97044
r-serori <ryunosuke.sakaue@gmail.com>
2024-07-31 07:09:29 +0900

    cors.php edit

--- config/cors.php
+++ config/cors.php
@@ -3,21 +3,53 @@
 return [
 
   'paths' => [
-    'api/*', 'sanctum/csrf-cookie', 'need'
+    'api/*',
+    'sanctum/csrf-cookie',
+    'login',
+    'register',
+    'forgotPassword',
+    'resetPassword',
+    'verify-email/*',
+    'updateInfo/*',
+    'vio-role',
+    'check-session',
+    'logout',
+    'getKey',
+    'ownerRegister',
+    'updateOwner',
+    'getOwner',
+    'showUser',
+    'getUsers',
+    'updatePermission',
+    'deleteUser',
+    'staffRegister',
+    'csrf-token',
+    'attendance_times/*',
+    'firstAttendanceTimes/*',
+    'customers/*',
+    'courses/*',
+    'options/*',
+    'merchandises/*',
+    'hairstyles/*',
+    'schedules/*',
+    'daily_sales/*',
+    'monthly_sales/*',
+    'yearly_sales/*',
+    'stocks/*',
+    'stock_categories/*',
   ],
 
-  'allowed_methods' => ['*'],
+  'allowed_methods' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
 
-  'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+  'allowed_origins' => [env('FRONTEND_URL')],
 
   'allowed_origins_patterns' => [],
 
   'allowed_headers' => ['*'],
 
-  'exposed_headers' => [],
+  'exposed_headers' => ['*'],
 
   'max_age' => 0,
 
   'supports_credentials' => true,
-
 ];
