fc00b21b6745bc08f6ebebcac4ea0a3f72d78a7b
r-serori <ryunosuke.sakaue@gmail.com>
2024-07-31 06:48:08 +0900

    seeder追加

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,50 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 use Illuminate\Database\Seeder;
+
+
 
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Seed the application's database.a
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-        // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+
+
+
+        $this->call([
+            UserSeeder::class,
+            OwnerSeeder::class,
+            StaffSeeder::class,
+        ]);
+
+
+        $this->call([
+            CourseSeeder::class,
+            MerchandiseSeeder::class,
+            OptionSeeder::class,
+            HairstyleSeeder::class,
+            CustomerSeeder::class,
+            StockCategorySeeder::class,
+            StockSeeder::class,
+            ScheduleSeeder::class,
+            AttendanceTimeSeeder::class,
+            // CourseCustomerSeeder::class,
+            // OptionCustomerSeeder::class,
+            // MerchandiseCustomerSeeder::class,
+            // HairstyleCustomerSeeder::class,
+            // CustomerUserSeeder::class,
+            // DailySaleSeeder::class,
+            // MonthlySaleSeeder::class,
+            // YearlySaleSeeder::class,
         ]);
     }
 }
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use App\Enums\Roles;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    DB::table('users')->insert([
+      'name' => 'owner123',
+      'email' => 'owner@hairmail.com',
+      'phone_number' => '09012345678',
+      'password' => Hash::make('Password_123'),
+      'role' => Roles::$OWNER,
+      'isAttendance' => 0,
+      'created_at' => now(),
+      'updated_at' => now(),
+      'email_verified_at' => now(), // Optional: if you want to mark the user as verified
+    ]);
+    DB::table('users')->insert([
+      'name' => 'manager123',
+      'email' => 'manager@hairmail.com',
+      'phone_number' => '08012345678',
+      'password' => Hash::make('Password_123'),
+      'role' => Roles::$MANAGER,
+      'isAttendance' => 0,
+      'created_at' => now(),
+      'updated_at' => now(),
+      'email_verified_at' => now(), // Optional: if you want to mark the user as verified
+    ]);
+    DB::table('users')->insert([
+      'name' => 'staff123',
+      'email' => 'staff@hairmail.com',
+      'phone_number' => '07012345678',
+      'password' => Hash::make('Password_123'),
+      'role' => Roles::$STAFF,
+      'isAttendance' => 0,
+      'created_at' => now(),
+      'updated_at' => now(),
+      'email_verified_at' => now(), // Optional: if you want to mark the user as verified
+    ]);
+
+    // User::factory()
+    //     ->count(37)
+    //     ->create();
+  }
+}
